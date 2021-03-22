3786df8fdb99724934e7237334ee4143a84f16a3
DaniLllera <dlleraa01@elpuig.xeill.net>
2021-03-22 20:35:30 +0100

    Añadidas politicas de privacidad

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,158 +1,182 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:login_app/login/politicas.dart';
 import 'package:login_app/login/register.dart';
 import 'package:login_app/main/home.dart';
 
 class login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
 
     return Scaffold(
-
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => register()),
-          );},
-        label: const Text('NEW ACCOUNT', style: TextStyle(fontSize: 10),),
-        icon: const Icon(Icons.add, size: 20,),
+          );
+        },
+        label: const Text(
+          'NEW ACCOUNT',
+          style: TextStyle(fontSize: 10),
+        ),
+        icon: const Icon(
+          Icons.add,
+          size: 20,
+        ),
         backgroundColor: Color.fromRGBO(212, 175, 55, 1),
       ),
-
       resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromRGBO(50, 57, 116, 1),
       body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Stack(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Stack(
+          children: <Widget>[
+            Column(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    SizedBox(height: 60,),
-                    //CineSeniC foto
-                    Align(
-                      child: Image.network(
-                        'https://i.ibb.co/pjXW9D6/cinesenic.png',
-                        scale: 4,
-                      ),
-                      alignment: Alignment(0,-0.8),
-                    ),
-                    SizedBox(height: 50,),
-
-                    Container(
-                      child: Text(
-                        'LOGIN',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 5.0
-                        ),
-                      ),
-                      alignment: Alignment.centerLeft,
-                    ),
-
-                    SizedBox(height: 30,),
-                    //Email Textfield
-                    Container(
-                      width: double.infinity,
-                      child: TextField(
-                        obscureText: false,
-                        decoration: new InputDecoration(
-                            enabledBorder: const UnderlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.white, width: 0.0),
-                            ),
-                            labelText: 'Email',
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            )
-                        ),
-                        style: TextStyle(color: Colors.white),),
-                    ),
-
-                    SizedBox(height: 10,),
-
-                    //Password Textfield
-                    Container(
-                      width: double.infinity,
-                      child: TextField(
-                        obscureText: true,
-                        decoration: new InputDecoration(
-                            enabledBorder: const UnderlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.white, width: 0.0),
-                            ),
-                            labelText: 'Password',
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            )
-                        ),
-                        style: TextStyle(color: Colors.white),),
-                    ),
-
-                    SizedBox(height: 12,),
-                    Container(
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12,
-                        ),
-                      ),
-                      alignment: Alignment.centerRight,
-                    ),
-
-                    SizedBox(height: 30,),
-
-                    Container(
-                      child: MaterialButton(
-                        minWidth: 200.0,
-                        height: 40.0,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => home()),
-                          );
-                        },
-                        color: Color.fromRGBO(50, 57, 116, 1),
-                        child: Text('LOGIN', style: TextStyle(color: Colors.white)),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                          side: BorderSide(color: Colors.white, width: 1),
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 10,),
-
-                    Container(
-                      child: Image.network(
-                        'https://i.ibb.co/d6GsSCP/pngegg.png',
-                        scale: 25,
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  height: 60,
                 ),
+                //CineSeniC foto
+                Align(
+                  child: Image.network(
+                    'https://i.ibb.co/pjXW9D6/cinesenic.png',
+                    scale: 4,
+                  ),
+                  alignment: Alignment(0, -0.8),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+
                 Container(
-                  alignment: Alignment.bottomLeft,
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
                   child: Text(
-                    'Terms & Conditions',
+                    'LOGIN',
                     style: TextStyle(
                         color: Colors.white,
-                        fontSize: 13
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 5.0),
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+                //Email Textfield
+                Container(
+                  width: double.infinity,
+                  child: TextField(
+                    obscureText: false,
+                    decoration: new InputDecoration(
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 0.0),
+                        ),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
+
+                //Password Textfield
+                Container(
+                  width: double.infinity,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: new InputDecoration(
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 0.0),
+                        ),
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
                     ),
                   ),
-                )
+                  alignment: Alignment.centerRight,
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+
+                Container(
+                  child: MaterialButton(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => home()),
+                      );
+                    },
+                    color: Color.fromRGBO(50, 57, 116, 1),
+                    child: Text('LOGIN', style: TextStyle(color: Colors.white)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      side: BorderSide(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
+
+                Container(
+                  child: Image.network(
+                    'https://i.ibb.co/d6GsSCP/pngegg.png',
+                    scale: 25,
+                  ),
+                ),
               ],
             ),
-          ),
-      );
+            Container(
+                alignment: Alignment.bottomLeft,
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => politicas()),
+                    );
+                  },
+                  child: Text(
+                    'Terms & Conditions',
+                    style: TextStyle(color: Colors.white, fontSize: 13),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/login/politicas.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class politicas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Politicas de Privacidad'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.all(30),
+                  child: Text(
+                    'Terms and Conditions',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(15),
+                  child: Text(
+                    'Cookies',
+                    style: TextStyle(fontSize: 20, color: Colors.orange),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    'Like any other website, CineSeniC.com uses cookies. These cookies are used to store information including visitors preferences, '
+                    'and the pages on the website that the visitor accessed or visited.\n\nThe information is used to optimize the users experience '
+                    'by customizing our web page content based on visitors browser type and/or other information.For more general information on cookies, '
+                    'please read "What Are Cookies" from Cookie Consent.',
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(15),
+                  child: Text(
+                    'Privacy Policies',
+                    style: TextStyle(fontSize: 20, color: Colors.orange),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    'CineSeniC.com Privacy Policy does not apply to other advertisers or websites. Thus,'
+                    ' we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. '
+                    'It may include their practices and instructions about how to opt-out of certain options.\n\nYou can choose to disable cookies through your individual browser options. '
+                    'To know more detailed information about cookie management with specific web browsers, it can be found at the browsers respective websites.',
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(15),
+                  child: Text(
+                    'Children\'s Information',
+                    style: TextStyle(fontSize: 20, color: Colors.orange),
+                  )),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    'Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, '
+                        'and/or monitor and guide their online activity.\n\nCineSeniC.com does not knowingly collect any Personal Identifiable Information from children under the age of 13.'
+                        ' If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly '
+                        'remove such information from our records.',
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
