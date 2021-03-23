01c80c408cdd80f8862c0d37163131611bc50853
Xyphrs <dromerof01@elpuig.xeill.net>
2021-03-23 20:53:16 +0100

    Firebase conection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_app/login/login.dart';
 
-void main() => runApp(MaterialApp(home: login(), debugShowCheckedModeBanner: false, ));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print('Error');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          print('Conectado a Firebase');
+          return login();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        print('Conectando');
+        return login();
+      },
+      ),
+    );
+  }
+}
