078c28942f0d3243171b0b3029f333addcf91f14
rogerroque <reroqueo01@elpuig.xeill.net>
2021-05-27 19:57:01 +0200

    afsdf

--- lib/Classes/promotions.dart
+++ lib/Classes/promotions.dart
@@ -30,14 +30,39 @@ class promotions extends StatelessWidget {
                         height: 80,
                         color: Colors.yellow[600],
                         child: Container(
+                          padding: EdgeInsets.only(right: 10, bottom: 10, top: 10, left: 10),
                           alignment: Alignment.bottomRight,
-                          child: Container(
-                              padding: EdgeInsets.only(right: 10, bottom: 10),
-                              child: Text("Code: " + promotionsList[index].code, style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Colors.white
-                              ),)
+                          child: Row(
+                            children: <Widget> [
+
+                              Container(
+                                child: Text(promotionsList[index].name, style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey[750]
+                                ),),
+                              ),
+
+                              Expanded(
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Column(
+                                      children: <Widget> [
+                                        Expanded(
+                                          child: Text("Code: " + promotionsList[index].code, style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                              color: Colors.grey[650]
+                                          ),),
+                                        ),
+                                        Text("Discount: " + promotionsList[index].percentage.toString() + "%", style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                            color: Colors.grey[650]
+                                        ),)
+                                      ]
+                                  ),
+                                ),
+                              ),
+                            ]
                           ),
                         )
                     );
